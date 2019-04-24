71d0028a53b96567fa65c08e236bd62250f6bae3
Simon Oehrl <oehrl@vr.rwth-aachen.de>
2019-04-24 11:11:59 +0200

    Use execFileSync for webpack

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,13 +1,12 @@
 var ghpages = require('gh-pages');
 
-const { spawnSync } = require('child_process');
+const { spawnSync, execFileSync } = require('child_process');
 const npmBin = spawnSync('npm', ['bin'], {
   encoding: 'utf8'
 });
 
-const webpack = spawnSync(npmBin.stdout.replace('\n', '') + '/webpack', ['--config', 'webpack.config.js'], {
-  encoding: 'utf8'
+execFileSync(npmBin.stdout.replace('\n', '') + '/webpack', ['--config', 'webpack.config.js'], {
+  encoding: 'utf8',
 });
-console.log(webpack.stdout);
 
 ghpages.publish('dist');
